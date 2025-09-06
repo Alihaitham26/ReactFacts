776e9a4a30a96b9ab01eb01f71103731f7805fb6
alihaitham26 <alihaithamahmed26@gmail.com>
2025-09-06 05:38:06 +0300

    build the page structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,6 @@
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"))
 
-function Page(){
-  return (
-    <main>
-      <header>
-        <img src="/react.svg" alt="" width="40px" />
-      </header>
-      <h1>why i love learning react</h1>
-      <ol>
-        <li>it is easy</li>
-        <li>will make me more employable</li>
-        <li>I need money</li>
-      </ol>
-    </main>
-  )
-}
-
-root.render(<Page/>)
+root.render(<App />)
