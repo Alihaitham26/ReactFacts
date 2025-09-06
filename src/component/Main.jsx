f3098b03d6c5b5d98a24d0031f6434558f50b7c4
alihaitham26 <alihaithamahmed26@gmail.com>
2025-09-06 06:52:32 +0300

    Build main component (React + CSS)

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main(){
     return (
-        <h1>this is main</h1>
+        <main>
+            <h2>Fun facts about React</h2>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200k stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apss, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
